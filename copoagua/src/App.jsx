8ac41ab6e3c6abe21248720ec50ec9f64e4a63d8
pprohem <pprg00@gmail.com>
2022-11-01 16:59:15 -0300

    atualizacao

--- copoagua/src/App.jsx
+++ copoagua/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import copo from './assets/img/aagua.png'
-
+import copo from './assets/img/agua1.webp'
+import './components/styles.css'
 import './App.css'
 
 function App() {
@@ -9,15 +9,25 @@ function App() {
   const [total, setTotal] = useState(0)
   const [meta, setMeta] = useState(2000)
   const [mensagem,setMensagem] = useState("")
+  const [cups, setCups] = useState([])
  
+  function drink(numeroCopo) {
+    const dataHora = new Date
+    setCups([
+      ...cups, 
+      {cup: numeroCopo, dataHora}
+    ])
+  }
 const calcular = () => {
  
    if(quantidade > 0){
     if(meta>0){
-      setCount(prevState => prevState+1)
-      setTotal(quantidade*(count+1))
+      const countTemp = count+1
+      setCount(countTemp)
+      setTotal(quantidade*countTemp)
       setMensagem("")
       calcularMeta()
+      drink(countTemp)
     }else{
       setMensagem("Não é possivel informar uma quantidade negativa")
     }
@@ -29,33 +39,70 @@ const zerarTotal = () => {
   setTotal(0)
   setCount(0)
   setMensagem("")
+  setCups([])
 }
 
 const calcularMeta = () => {
   if(quantidade*(count+1) >= meta){
     setMensagem("Você concluiu sua meta diaria!")
-  } else {
-    
+  } 
+  
 
-  }
+
+
+
 }
-
+console.log(cups)
   return (
-    <div className="App">
-      <img src={copo} alt="" />
-      <h1>Quantos copos foram bebidos?</h1>
+    <body>
+      
+      <div className="App">
       
       
-      <h2>{count} copos</h2>
-      <h3> O total de agua ingerido foi {total} ml</h3>
-      <h3>{mensagem}</h3>
-      <input type="number" min={0} value={quantidade} placeHolder="Informe a capacidade do copo.."  onChange={e => setQuantidade(parseInt(e.target.value))}/>
-      <input type="number" min={0} value={meta}placeHolder="Informe a meta de agua a se ingerir"  onChange={e => setMeta(parseInt(e.target.value))}/>
-      <button onClick={calcular}> Clique aqui pra somar quantos copos foram bebidos</button>
-      <button onClick={zerarTotal}>Clique aqui para zerar a quantidade de copos bebidos.</button>
+      
+      <div className="inputs"> 
+        
+        
+            <h3>Informe a capacidade do copo (ML)</h3>
+                <input type="number" min={0} 
+                          value={quantidade} placeHolder="Informe a capacidade do copo.."  
+                       onChange={e => setQuantidade(parseInt(e.target.value))}/>
+                <h3>Informe sua meta de ingestão diária(ML)</h3>
+                <input type="number" min={0} 
+                      value={meta}placeHolder="Informe a meta de agua a se ingerir" 
+                      onChange={e => setMeta(parseInt(e.target.value))}/>
+          
+            
+              <h3>"Diario de Água"</h3>
+            {cups.map((copo,index)=><ul key={index}>   
+                    <li >Copo N:{copo.cup}││{copo.dataHora.toString()}</li>
+             </ul>
+            )}
+       
+
+        
+        
+      </div>
+      
+     
+      <div className="texto"> 
+        <h1 className='qtdCopos'>Quantos copos foram bebidos?</h1>
+        <h2 className='totalCopos'>{count} copos</h2>
+        <h3> O total de agua ingerido foi {total} ml</h3>
+        <h3>{mensagem}</h3>
+        
+        <div className="botoes">
+        <button onClick={calcular}>+1 COPO</button>
+        <button onClick={zerarTotal}>Clique aqui para zerar a quantidade de copos bebidos.</button>
+        </div>
+        
+        
+      </div>
       
      
     </div>
+    </body>
+    
     
   )
 }
